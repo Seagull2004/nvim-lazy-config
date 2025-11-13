151d84183e457f954f6c623fc6b809ed2649cc92
Seagull2004 <sea2004gull@gmail.com>
2025-11-13 10:16:22 +0100

    versione con mason configurato in manuale (con specifico file)

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -1,6 +1,6 @@
 return {
   { "akinsho/bufferline.nvim", enabled = false },
-  { "nvim-treesitter/nvim-treesitter" , enabled = false },
-  { "nvim-treesitter/nvim-treesitter-textobjects" , enabled = false },
-  { "nvim-treesitter/nvim-treesitter-context" , enabled = false },
+  -- { "nvim-treesitter/nvim-treesitter" , enabled = false },
+  -- { "nvim-treesitter/nvim-treesitter-textobjects" , enabled = false },
+  -- { "nvim-treesitter/nvim-treesitter-context" , enabled = false },
 }
