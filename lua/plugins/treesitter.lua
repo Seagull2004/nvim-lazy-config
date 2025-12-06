7c99ac0a72648141bdbb22e36c2fe471679c9820
Seagull2004 <sea2004gull@gmail.com>
2025-12-06 12:58:39 +0100

    nuova config di treesitter (con haskell)

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -3,7 +3,41 @@ return {
   opts = {
     ensure_installed = {
       "haskell",
+      "bash",
+      "c",
+      "diff",
+      "html",
+      "javascript",
+      "jsdoc",
+      "json",
+      "jsonc",
+      "lua",
+      "luadoc",
+      "luap",
+      "markdown",
+      "markdown_inline",
+      "printf",
+      "python",
+      "query",
+      "regex",
+      "toml",
+      "tsx",
+      "typescript",
+      "vim",
+      "vimdoc",
+      "xml",
+      "yaml",
     },
+    auto_install = false,
+    highlight = {
+      enable = true,
+      disable = function (lang,buf)
+        local max_filesize = 100 * 1024 -- 100KB
+        local ok,stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+        if ok and stats and stats.size > max_filesize then
+          return true
+        end
+      end,
+    }
   },
 }
-
