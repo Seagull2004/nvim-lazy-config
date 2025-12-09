94ddb1c6838428157269532bfdcb0d08e4462818
Seagull2004 <sea2004gull@gmail.com>
2025-12-09 22:16:29 +0100

    neovim btw

--- lua/plugins/snack.lua
+++ lua/plugins/snack.lua
@@ -7,6 +7,7 @@ return {
     picker = {
       sources = {
         explorer = {
+          auto_open = false,
           layout = {
             layout = {
               position = "right",
@@ -15,5 +16,34 @@ return {
         },
       },
     },
+    dashboard = {
+      preset = {
+        pick = function(cmd, opts)
+          return LazyVim.pick(cmd, opts)()
+        end,
+        header = [[
+ ███╗   ██╗██╗   ██╗██╗███╗   ███╗    ██████╗ ████████╗██╗    ██╗
+ ████╗  ██║██║   ██║██║████╗ ████║    ██╔══██╗╚══██╔══╝██║    ██║
+ ██╔██╗ ██║██║   ██║██║██╔████╔██║    ██████╔╝   ██║   ██║ █╗ ██║
+ ██║╚██╗██║╚██╗ ██╔╝██║██║╚██╔╝██║    ██╔══██╗   ██║   ██║███╗██║
+ ██║ ╚████║ ╚████╔╝ ██║██║ ╚═╝ ██║    ██████╔╝   ██║   ╚███╔███╔╝
+╚═╝  ╚═══╝  ╚═══╝  ╚═╝╚═╝     ╚═╝    ╚═════╝    ╚═╝    ╚══╝╚══╝
+]],
+        -- stylua: ignore
+        ---@type snacks.dashboard.Item[]
+        keys = {
+          { icon = " ", key = "f", desc = "Find File", action = ":lua Snacks.dashboard.pick('files')" },
+          { icon = " ", key = "n", desc = "New File", action = ":ene | startinsert" },
+          { icon = " ", key = "g", desc = "Find Text", action = ":lua Snacks.dashboard.pick('live_grep')" },
+          { icon = " ", key = "r", desc = "Recent Files", action = ":lua Snacks.dashboard.pick('oldfiles')" },
+          { icon = " ", key = "c", desc = "Config", action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})" },
+          { icon = " ", key = "s", desc = "Restore Session", section = "session" },
+          { icon = " ", key = "x", desc = "Lazy Extras", action = ":LazyExtras" },
+          { icon = "󰒲 ", key = "l", desc = "Lazy", action = ":Lazy" },
+          { icon = " ", key = "q", desc = "Quit", action = ":qa" },
+        },
+      },
+    },
   },
 }
+
