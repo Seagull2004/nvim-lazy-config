33166045cd8f3d7cdb2434048bba04ebc5db0e73
Seagull2004 <sea2004gull@gmail.com>
2026-03-22 13:47:39 +0100

    mouse scroll extra granularity

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -12,3 +12,13 @@ vim.g.minipairs_disable = true
 vim.opt.guicursor = "n-v-c:block,i-ci-ve-t:ver25,r-cr:hor20"
 
 vim.opt.clipboard = ""
+
+vim.cmd("set spelllang=en_us,it_IT")
+
+  -- Map mouse wheel to scroll one line at a time
+vim.api.nvim_set_keymap('n', '<ScrollWheelUp>', '<C-y>', { silent = true })
+vim.api.nvim_set_keymap('n', '<ScrollWheelDown>', '<C-e>', { silent = true })
+vim.api.nvim_set_keymap('i', '<ScrollWheelUp>', '<C-o><C-y>', { silent = true })
+vim.api.nvim_set_keymap('i', '<ScrollWheelDown>', '<C-o><C-e>', { silent = true })
+vim.api.nvim_set_keymap('v', '<ScrollWheelUp>', '<C-y>', { silent = true })
+vim.api.nvim_set_keymap('v', '<ScrollWheelDown>', '<C-e>', { silent = true })
